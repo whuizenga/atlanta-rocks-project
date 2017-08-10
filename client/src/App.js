9f2cfa67aa52a082a01aee391c4fa9f239d699f7
whuizenga <will.huizenga@gmail.com>
2017-08-10 16:16:22 -0400

    made a link to my search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
+import styled from 'styled-components';
 import LoginButton from './Components/Login';
 import Homepage from './Components/Homepage.jsx';
 
 
 class App extends Component {
   render() {
+  const NavBar = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+  `
     return (
       <Router>
       <div>
       <div>
-        <nav>
+        <NavBar>
           <Link to="/">Home</Link>
           <LoginButton />
-        </nav>
+        </NavBar>
       </div>
       <div>
         <Route exact path="/" component={Homepage} />
--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 class Homepage extends Component {
     render() {
+        const TitleDiv = styled.div`
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            justify-content: center;
+            align-items: center;
+            h1{
+            font-size: 60px;
+            }
+        `
         return (
             <div>
-                Sup, I'ma homepage
+                <TitleDiv>
+                <h1>Atlanta Rocks</h1>
+
+                <Link to="/search">climb on</Link>
+                </TitleDiv>       
             </div>
         );
     }
