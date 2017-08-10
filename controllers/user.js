4b55a92cb949371767f7793e3ef8f5505f7ebd5f
whuizenga <will.huizenga@gmail.com>
2017-08-10 19:31:06 -0400

    Can send user login information to the server and get back responses if no user, wrong pass, or login success

--- controllers/user.js
+++ controllers/user.js
@@ -8,4 +8,23 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/login/:username/:password", (req, res) => {
+    const username = req.params.username;
+    const password = req.params.password;
+
+    User.find().then((users) => {
+        const userIWant = users.find((user) => {
+            return user.username === username
+        })
+        if (userIWant.password === password){
+            res.send("success")
+        } else {
+            res.send("Wrong Password")
+        }
+        res.json(userIWant);
+    })
+        .catch((err) => {
+            res.send("User does not exist");
+        })
+})
 module.exports = router;
