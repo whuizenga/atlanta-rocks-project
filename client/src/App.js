e3c85f9658dcb70b21af19772a56932726cbfbdd
whuizenga <will.huizenga@gmail.com>
2017-08-10 16:06:00 -0400

    express router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import LoginButton from './Components/Login';
+import Homepage from './Components/Homepage.jsx';
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
-        Hello World -React
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <LoginButton />
+        </nav>
       </div>
+      <div>
+        <Route exact path="/" component={Homepage} />
+      </div>
+      </div>
+      </Router>
     );
   }
 }
---
+++ client/src/Components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                Sup, I'ma homepage
+            </div>
+        );
+    }
+}
+
+export default Homepage;
