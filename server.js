be3e49f592beb3561bce9d165503b26f6fa4ffeb
whuizenga <will.huizenga@gmail.com>
2017-08-11 13:53:17 -0400

    fixed spelling error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const UsersController = require('./Controllers/user');
+const UsersController = require('./controllers/user');
 const RouteController = require('./controllers/route')
 const app = express();
 mongoose.Promise = global.Promise;
