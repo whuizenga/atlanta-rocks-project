1afbf0d939246450464d4ba122c08afbf625974a
whuizenga <will.huizenga@gmail.com>
2017-08-11 15:49:10 -0400

    can now search by difficulty

--- client/src/Components/ShowDifficulty.jsx
+++ client/src/Components/ShowDifficulty.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-class ShowDifficulty extends Component {
+import RouteList from './RouteList';
+
+class ShowWall extends Component {
+    constructor(){
+        super()
+        this.state={
+            difficulty: "",
+            routes: [],
+        }
+    }
+
+    componentWillMount(){
+        const searchParam = this.props.match.params.diffId;
+
+        axios.get(`/api/route/difficultySearch/${searchParam}`)
+            .then((res) => {
+                console.log(res.data)
+                const newState = {...this.state};
+                newState.routes = res.data;
+                newState.wallNumber = searchParam;
+                this.setState(newState);
+            })
+    }
+
     render() {
+        const WallWrapper = styled.div`
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        `
         return (
             <div>
-                Show a list of the routes by difficulty here.
+                <WallWrapper>
+                <h1>Difficulty {this.state.difficulty}</h1>
+
+                {this.state.routes.map((route, index) => {
+                    if(!route.date_retired){
+                    return <RouteList key={index}
+                            route={route}/>
+                } else {
+                    return null;
+                }})}
+                </WallWrapper>
             </div>
         );
     }
 }
 
-export default ShowDifficulty;
+export default ShowWall;
